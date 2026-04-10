8951d2e612b9dd971a2e9217fb1691acd8fec87e
hank187548 <hank187548@gmail.com>
2026-04-10 15:29:55 +0800

    Time: 19 ms (61.45%), Space: 19.3 MB (65.26%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,17 +1,56 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
 
-        len1, len2 = len(s1), len(s2)
+        # len1, len2 = len(s1), len(s2)
 
-        if len1 > len2 :
+        # if len1 > len2 :
+        #     return False
+        
+        # sorted_s1 = sorted(s1)
+
+        # for i in range(len2 - len1 + 1):
+        #     sorted_str = sorted(s2[i:i + len1:])
+
+        #     if sorted_s1 == sorted_str:
+        #         return True
+        
+        # return False
+
+        if len(s1) > len(s2):
             return False
-        
-        sorted_s1 = sorted(s1)
 
-        for i in range(len2 - len1 + 1):
-            sorted_str = sorted(s2[i:i + len1:])
+        s1Count, s2Count = [0]*26, [0]*26
 
-            if sorted_s1 == sorted_str:
+        for i in range(len(s1)):
+            s1Count[ord(s1[i])- ord('a')] += 1
+            s2Count[ord(s2[i])- ord('a')] += 1
+
+
+        matches = 0
+        for i in range(26):
+            matches += (1 if s1Count[i] == s2Count[i] else 0)
+
+        if matches == 26:
+            return True
+
+        l = 0
+        for r in range(len(s1), len(s2)):
+            if matches == 26:
                 return True
-        
-        return False
+
+            index = ord(s2[r]) - ord('a')
+            s2Count[index] += 1  
+            if s1Count[index] == s2Count[index]:
+                matches += 1          
+            elif s1Count[index] + 1 == s2Count[index]:
+                matches -= 1
+
+            index = ord(s2[l]) - ord('a')
+            s2Count[index] -= 1
+            if s1Count[index] == s2Count[index]:
+                matches += 1
+            
+            elif s1Count[index] - 1 == s2Count[index]:
+                matches -= 1     
+            l += 1       
+        return matches == 26
