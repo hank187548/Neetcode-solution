10916d73649ce1a453934efbde17381698390c4f
hank187548 <hank187548@gmail.com>
2026-04-13 21:43:52 +0800

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,11 +1,44 @@
 class Solution:
-    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        heap = []
+    def maxSlidingWindow(self, nums: list[int], k: int) -> list[int]:
+        # if not nums:
+        #     return []
+            
+        # max_in_window = max(nums[0:k])
+        # max_list = [max_in_window]
+        # l = 1
+        
+        # while l <= len(nums) - k:
+            
+        #     old_out = nums[l - 1]
+        #     new_in = nums[l + k - 1]
+            
+        #     if new_in >= max_in_window:
+        #         max_in_window = new_in
+                
+        #     elif old_out == max_in_window:
+        #         max_in_window = max(nums[l : l+k])
+                
+            
+        #     max_list.append(max_in_window)
+            
+        #     l += 1
+
+        # return max_list
+
         output = []
-        for i in range(len(nums)):
-            heapq.heappush(heap, (-nums[i], i))
-            if i >= k - 1:
-                while heap[0][1] <= i - k:
-                    heapq.heappop(heap)
-                output.append(-heap[0][0])
+        q = deque()
+        l = r = 0
+        while r < len(nums):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+
         return output
